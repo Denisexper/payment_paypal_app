e78d1d8c513522e67194a7df1ad04f0b97ffbbc3
Denisexper <clasic.exper2023@gmail.com>
2025-11-21 19:04:47 -0600

    feat: jwt in routes

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,6 @@
 import { usersModel } from "../models/users.model.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 export class usersController {
 
@@ -47,9 +49,47 @@ export class usersController {
             });
             }
 
+            const token = jwt.sign(
+                {user: user._id, email: user.email},
+                process.env.JWT_SECRET,
+                {expiresIn: process.env.JWT_EXPIRES}
+            )
+
             res.status(200).json({
                 msj: "welcome in",
-                data: response
+                data: response,
+                token
+            })
+        } catch (error) {
+            
+            res.status(500).json({
+                msj: "uknow error",
+                error: error.message
+            })
+        }
+    }
+
+    async register (req, res) {
+
+        const { name, email, password } = req.body;
+
+        try {
+            
+            const hasPass = bcrypt.hash(password, 10)
+
+            const response = await usersModel.create({name, email, hasPass})
+
+            //crear un token a la hora de hacer register
+            const token = jwt.sign(
+                {id: user._id, email: user.email},
+                process.env.JWT_SECRET,
+                {expiresIn: process.env.JWT_EXPIRES}
+            )
+
+            res.status(200).json({
+                msj: "user created",
+                data: response,
+                token
             })
         } catch (error) {
             
---
+++ src/routes/users.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { usersController } from "../controllers/users.controller.js";
+import { authRequired } from "../auth/authMiddleware.js";
+
+const router = Router()
+const controller = new usersController()
+
+router.get("/get-users", authRequired, controller.getUsers)
